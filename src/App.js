0184a2413077ee83d435a12407f344afe46bd4bb
minseokKang <min1042004444@gmail.com>
2023-11-08 17:17:01 +0900

    App.js 수정 금지

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import StackNavigation from './navigation/StackNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-
+    // App.js는 수정하지 말 것
     return (
     <SafeAreaProvider>
         <NavigationContainer>

03b12caefa0bcf59e696688004cdad4be4dab70e
고에릭 <me@Mexavas.local>
2023-11-24 00:07:00 +0900

    회원가입과 로그인 기능

--- src/profile/ProfilePage.js
+++ src/profile/ProfilePage.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View,} from 'react-native';
+import { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './../../firebaseConfig';
+import {LoggedIn} from './components/ProfileSettings';
+import Login from './components/LogIn';
+import Signup from './components/SignUp';
+import ResetPassword from './components/ResetPassword';
 const ProfilePage = () => {
-    return (
-        <View>
-            <Text>프로필 페이지</Text>
-        </View>
-    );
-};
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [screen, setScreen] = useState(null);
+  
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+      
+    const getScreen = () => {
+        if (loggedIn) return <LoggedIn />;
+        if (screen === 'signup') return <Signup setScreen={setScreen} />;
+        if (screen === 'reset-password') return <ResetPassword setScreen={setScreen} />;
+        return <Login setScreen={setScreen} />;
+      };
+    
+      return <View style={{ flex: 1 }}>{getScreen()}</View>;
+    }
+          
 
 export default ProfilePage;

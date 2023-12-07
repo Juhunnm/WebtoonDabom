2519fb1152030a738c84a318d2e660bdce48e161
juhun <minjuhun1@gmail.com>
2023-12-07 15:11:07 +0900

    컴포넌트 명 수정

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -1,9 +1,15 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+
+// 아이콘
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import BottomTabNavigation from './BottomTabNavigation';
 
+// Home 관련 컴포넌트
+import WebtoonDetailPage from '../home/WebtoonDetailPage';
+import SearchPage from '../home/SearchPage';
 // 로그인 관련 컴포넌트
 import ResetPassword from '../profile/components/ResetPassword';
 import Signup from '../profile/components/SignUp';
@@ -14,18 +20,31 @@ const Stack = createStackNavigator();
 
 const StackNavigation = () => {
     const ref = useRef(null);
-  return (
-    <Stack.Navigator>
+
+
+
+
+    return (
+        <Stack.Navigator>
             {/* 메인 페이지 그룹 */}
             <Stack.Group>
                 <Stack.Screen name="Home" component={BottomTabNavigation} options={{
-                    title: '웹툰다봄',
-                    headerLeft: null, //뒤로가기 버튼을 없앰
-                    headerRight: () => (//헤더바 오른쪽에 배치할 놈들
-                        <View style={styles.headerRight}>
-                            <MaterialCommunityIcons name="bell" size={24} color="black" style={styles.icons} />
-                        </View>
-                    )
+                    headerShown: false,
+                }} />
+                <Stack.Screen name="WebtoonDetailPage" component={WebtoonDetailPage} options={{
+                    title: '웹툰 정보',
+                    headerTintColor: 'black',
+                    headerBackTitleVisible: false, // 뒤로가기 버튼 옆의 텍스트를 표시하지 않음
+                    headerStyle: {
+                        borderBottomWidth: 0, // 헤더바의 하단 border를 없앰
+                        elevation: 0, // 안드로이드에서의 shadow 없앰
+                        shadowOpacity: 0, // iOS에서의 shadow 없앰
+                    },
+                }} />
+                <Stack.Screen name="SearchPage" component={SearchPage} options={{
+                    title: '',
+                    headerTintColor: 'black',
+                    headerBackTitleVisible: false, // 뒤로가기 버튼 옆의 텍스트를 표시하지 않음
                 }} />
             </Stack.Group>
             {/* 로그인 페이지 그룹 */}
@@ -63,7 +82,7 @@ const StackNavigation = () => {
                 }} />
             </Stack.Group>
         </Stack.Navigator>
-  )
+    )
 }
 
 const styles = StyleSheet.create({
